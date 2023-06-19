0ac5ca6c3da1118b31ac7c140a8bd2ef91b54b7f
smhrd <smhrd@host.docker.internal>
2023-06-19 17:52:48 +0900

    23.06.19.1752 kyb 모달 수정 push

--- src/main/webapp/resources/js/upload.js
+++ src/main/webapp/resources/js/upload.js
@@ -300,11 +300,11 @@ let allDetection = () => {
 				$(".modalLoading").fadeIn();
 			},
 			complete: function() {
-				// 타이머 리셋
-				resetStopwatch();
-				
 				// 로딩 중 모달창 없애기
 				$(".modalLoading").fadeOut();
+				
+				// 타이머 리셋
+				resetStopwatch();
 			},
 			error: function(e) {
 				console.log("에러")
